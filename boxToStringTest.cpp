04aa7852018940d83a92cbc3edc4747ac4c25877
Jorge Medina <jim@umail.ucsb.edu>
2021-04-21 16:07:16 -0700

    implemented box to string function

--- boxToStringTest.cpp
+++ boxToStringTest.cpp
@@ -1,24 +1,19 @@
 #include "shapes.h"
 #include "shapeFuncs.h"
 #include "tddFuncs.h"
-
+using namespace std;
 int main()
 {
 
-   struct Point p1, p2;
-
-   initPoint(&p1, 3.0, 4.0);
-   assertEquals("(3,4)", pointToString(p1), "pointToString(p1)");
-
-   initPoint(&p2, 3.14159, 6.2831853071);
-   assertEquals("(3.14,6.28)", pointToString(p2), "pointToString(p2)");
-   assertEquals("(3,6)", pointToString(p2, 1), "pointToString(p2,1)");
-   assertEquals("(3.142,6.283)", pointToString(p2, 4), "pointToString(p2,4)");
-   assertEquals("(3.1416,6.2832)", pointToString(p2, 5), "pointToString(p2,5)");
-
-
    Box b1;
-   initBox
+   initBox(&b1, 3.123456, 4.123456, 10, 7);
+
+  // assertEquals("ul=(3,4), w=10,h=7", boxToString(b1,1), "boxToString(b1,1)");
+   assertEquals("ul=(3.1,4.1), w=10,h=7", boxToString(b1,2), "boxToString(b2,2)");
+   assertEquals("ul=(3.12,4.12), w=10,h=7", boxToString(b1,3), "boxToString(b3,3)");
+   assertEquals("ul=(3.123,4.123), w=10,h=7", boxToString(b1,4), "boxToString(b4,4)");
+   assertEquals("ul=(3.1235,4.1235), w=10,h=7", boxToString(b1,5), "boxToString(b5,5)");
+   assertEquals("ul=(3.12346,4.12346), w=10,h=7", boxToString(b1,6), "boxToString(b6,6)");
 
 
 
